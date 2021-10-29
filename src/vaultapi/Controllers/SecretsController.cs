e0d42f3da5cbe4bb3ee26ca570588d1062f02022
romeelk <romeel_k@yahoo.co.uk>
2021-10-29 16:34:06 +0100

    updated gitignore

---
+++ src/vaultapi/Controllers/SecretsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace vaultapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretsController : ControllerBase
+    {
+    
+
+        private readonly ILogger<SecretsController> _logger;
+
+        public SecretsController(ILogger<SecretsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var rng = new Random();
+            return "OK";
+        }
+    }
+}
